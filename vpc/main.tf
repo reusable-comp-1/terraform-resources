f99944e5f195ab007281dfe340c3b09e279505aa
reusable-comp-1 <96808184+reusable-comp-1@users.noreply.github.com>
2023-08-10 11:04:06 +0530

    Add files via upload

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,9 +1,55 @@
-resource "aws_vpc" "vpc" {
-  
-  
-  cidr_block       = var.cidr_block
-  instance_tenancy = "default"
-  Name= "test-vpc"
-  
+resource "google_compute_network" "gcp-vpc" {
+  name                    = var.name
+  auto_create_subnetworks = false
 }
+
+resource "google_compute_subnetwork" "public-subnet" {
+   name          = "${var.public-subnet}"
+   ip_cidr_range = "${var.vpc-cidr}"
+   region        = "${var.region}"
+   network       = "${google_compute_network.gcp-vpc.id}"
+}
+
+resource "google_compute_subnetwork" "private-subnet" {
+   name          = "${var.private-subnet}"
+   ip_cidr_range = "${var.vpc_cidr}"
+   region        = "${var.region}"
+   network       = "${google_compute_network.gcp-vpc.id}"
+}
+
+resource "google_compute_router" "router" {
+  name    = "my-router"
+  #region  = google_compute_subnetwork.private-subnet.region
+  network = google_compute_network.gcp-vpc.id
+  bgp {
+    asn            = 64514
+    advertise_mode = "CUSTOM"
+  }
+}
+
+# resource "google_compute_router_nat" "nat" {
+#   name                               = "my-router-nat"
+#   router                             = google_compute_router.router.name
+#   region                             = google_compute_router.router.region
+#   nat_ip_allocate_option             = "AUTO_ONLY"
+#   source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+#   subnetwork {
+#     name                    = "private"
+#     source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+#   }
+# }
+
+
+# resource "google_compute_subnetwork" "subnet" {
+#    for_each      = toset(var.subnet_names_set)
+#    name          = each.value    
+#   #name          = "${var.subnet_names}-subnet"
+#   ip_cidr_range = var.subnet_cidr[0]
+#   network       = google_compute_network.main.id
+#   region        = var.region
+  
+#   depends_on = [google_compute_network.main]
+# }
+
+
 
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,47 +1,32 @@
-
-
- variable "tags" {
-  type        = map(string)
-  default     = {
-    Name      = "vpc123"
-    environment = "test"
-}
+variable "name" {
+    type= string
+    default = "demo-vpc"
 }
 
-variable "cidr_block" {
+variable "public-subnet" {
   type = string
-   default = "10.0.0.0/16"
+  default = "public"
 }
-# variable "subnet_name" {
-#     type = list(string)
-#     #default = ["public", "private"]  
-# }
+
+variable "private-subnet" {
+  type = string
+  default = "private"
+}
 
 
+variable "vpc-cidr" {
+    type = string
+  default = "10.0.0.0/24"
 
-#  variable "azs" {
-#    type = list(string)
-#  }
+}
 
-#  variable "private_subnets" {
-#    type = list(string)
-#     default= ["10.0.101.0/24"]
-#  }
+variable "vpc_cidr" {
+    type = string
+    default = "10.0.1.0/24"
+  
+}
 
-#  variable "public_subnets" {
-#    type = list(string)
-#     default= ["10.0.1.0/24"]
-#  }
-
-
-
-#  variable "environment" {
-#     type = string
-#   default = "test"
-
-
-# }
-# variable "Name" {
-#   type = string
-#   default = "vpc123"
-# }
+variable "region" {
+  type=string
+  default = "asia-south1"  
+}
